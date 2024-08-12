b4f50c5ca4c7708c8cefdb6ecd070b5e39799101
WatcherMagic <oliviaw.randomnumber@gmail.com>
2024-08-12 16:50:26 -0600

    - Assembly security attribute added - Mod info added

--- SpearmasterSpearExplosionMain.cs
+++ SpearmasterSpearExplosionMain.cs
@@ -1,9 +1,17 @@
 ﻿using BepInEx;
+using System.Security.Permissions;
+
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 
 namespace Spearmaster_Spear_Explosion
 {
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
     public class SpearmasterSpearExplosionMain : BaseUnityPlugin
     {
+        public const string PLUGIN_GUID = "watchermagic.sm_spear_explosion";
+        public const string PLUGIN_NAME = "Spearmaster Spear Explosion";
+        public const string PLUGIN_VERSION = "1.0.0";
+
         private void OnEnable()
         {
 
