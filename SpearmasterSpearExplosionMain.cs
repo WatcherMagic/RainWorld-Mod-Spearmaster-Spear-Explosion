c85ead7afbf11188b7d9255d5cff420a03791b05
WatcherMagic <oliviaw.randomnumber@gmail.com>
2024-09-06 17:26:00 -0600

    changed mod id and version number

--- SpearmasterSpearExplosionMain.cs
+++ SpearmasterSpearExplosionMain.cs
@@ -8,9 +8,9 @@ namespace Spearmaster_Spear_Explosion
     [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
     public class SpearmasterSpearExplosionMain : BaseUnityPlugin
     {
-        public const string PLUGIN_GUID = "watchermagic.sm_spear_explosion";
+        public const string PLUGIN_GUID = "sm_spear_explosion";
         public const string PLUGIN_NAME = "Spearmaster Spear Explosion";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "0.1";
 
         private void OnEnable()
         {
