f111df58b774d04c926270fc48da97fd53612571
WatcherMagic <oliviaw.randomnumber@gmail.com>
2024-09-06 18:53:53 -0600

    added Player.Update hook; for some reason Logger.LogMessage causes NullReference exception and prevents campaign from loading

--- SpearmasterSpearExplosionMain.cs
+++ SpearmasterSpearExplosionMain.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
 using System.Security.Permissions;
 
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
@@ -14,7 +15,15 @@ namespace Spearmaster_Spear_Explosion
 
         private void OnEnable()
         {
+            On.Player.Update += TakeCustomInput;
+        }
+
+        private void TakeCustomInput(On.Player.orig_Update orig, Player self, bool eu)
+        {
+
+            orig(self, eu);
 
         }
+
     }
 }
