18a0ab2a672ffa64d1589eceb046d8f1f843a572
WatcherMagic <oliviaw.randomnumber@gmail.com>
2024-09-07 14:15:33 -0600

    Added PlayerGraphics var for tail specks, unconfirmed whether it works properly but compiles
    Noted issues:
    - ability activates when pckp is pressed soon after being held
    - items swap hands when ability activates
    - CURRENTLY PREVENTS SPEARMASTER FROM MAKING SPEARS NORMALLY

--- SpearmasterSpearExplosionMain.cs
+++ SpearmasterSpearExplosionMain.cs
@@ -2,6 +2,7 @@
 using MoreSlugcats;
 using UnityEngine;
 using System.Security.Permissions;
+using System.Drawing;
 
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 
@@ -17,6 +18,42 @@ namespace Spearmaster_Spear_Explosion
         private void OnEnable()
         {
             On.Player.GrabUpdate += TakeCustomInput;
+            On.PlayerGraphics.TailSpeckles.setSpearProgress += GrowSpears;
+        }
+
+        private void TakeCustomInput(On.Player.orig_GrabUpdate orig, Player self, bool eu)
+        {
+
+            if (ModManager.MSC && self.SlugCatClass == MoreSlugcatsEnums.SlugcatStatsName.Spear)
+            {
+
+                if (tapPressedOnce(self, 2) && !self.input[1].pckp) 
+                {
+
+                    if (self.input[0].pckp)
+                    {
+                        if (self.graphicsModule != null)
+                        {
+                            PlayerGraphics.TailSpeckles tailspecks = (self.graphicsModule as PlayerGraphics).tailSpecks;
+                            var room = self.room;
+                            var pos = self.mainBodyChunk.pos;
+                            var color = self.ShortCutColor();
+                            room.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 2f, 280f, 0.25f, self, 0.7f, 160f, 1f));
+                            room.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+                            room.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new UnityEngine.Color(1f, 1f, 1f)));
+                            room.AddObject(new ExplosionSpikes(room, pos, 14, 30f, 9f, 7f, 170f, color));
+                            room.AddObject(new ShockWave(pos, 330f, 0.045f, 5, false));
+
+                            room.ScreenMovement(pos, default, 1.3f);
+
+                        }
+
+                    }
+
+                }
+            }
+
+            orig(self, eu);
         }
 
         private bool tapPressedOnce(Player self, int n)
@@ -34,34 +71,9 @@ namespace Spearmaster_Spear_Explosion
             return false;
         }
 
-        private void TakeCustomInput(On.Player.orig_GrabUpdate orig, Player self, bool eu)
+        private void GrowSpears(On.PlayerGraphics.TailSpeckles.orig_setSpearProgress orig, PlayerGraphics.TailSpeckles self, float p)
         {
-            orig(self, eu);
-
-            if (ModManager.MSC && self.SlugCatClass == MoreSlugcatsEnums.SlugcatStatsName.Rivulet)
-            {
-                if (tapPressedOnce(self, 2) && !self.input[1].pckp) 
-                {
-
-                    if (self.input[0].pckp)
-                    {
-                        var room = self.room;
-                        var pos = self.mainBodyChunk.pos;
-                        var color = self.ShortCutColor();
-                        room.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 2f, 280f, 0.25f, self, 0.7f, 160f, 1f));
-                        room.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
-                        room.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
-                        room.AddObject(new ExplosionSpikes(room, pos, 14, 30f, 9f, 7f, 170f, color));
-                        room.AddObject(new ShockWave(pos, 330f, 0.045f, 5, false));
-
-                        room.ScreenMovement(pos, default, 1.3f);
-                        room.PlaySound(SoundID.Bomb_Explode, pos);
-                        room.InGameNoise(new Noise.InGameNoise(pos, 9000f, self, 1f));
-                    }
-
-                }
-
-            }
+            
         }
 
     }
