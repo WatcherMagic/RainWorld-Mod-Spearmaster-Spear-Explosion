63aa6a631f5625c35a25b10711feaa6da4591165
WatcherMagic <oliviaw.randomnumber@gmail.com>
2024-09-06 19:22:43 -0600

    Added statement to check for Spearmaster identity & confirmed dual input works with explosion code copied from SlugBase.
    Problems noted:
    - Pressing pckp + thrw will activate the mechanic AND throw any item in the player's hand

--- SpearmasterSpearExplosionMain.cs
+++ SpearmasterSpearExplosionMain.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx;
-using BepInEx.Logging;
+using MoreSlugcats;
+using UnityEngine;
 using System.Security.Permissions;
 
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
@@ -22,6 +23,25 @@ namespace Spearmaster_Spear_Explosion
         {
 
             orig(self, eu);
+
+            if (ModManager.MSC && self.SlugCatClass == MoreSlugcatsEnums.SlugcatStatsName.Spear)
+            {
+                if (self.input[0].pckp && self.input[0].thrw)
+                {
+                    var room = self.room;
+                    var pos = self.mainBodyChunk.pos;
+                    var color = self.ShortCutColor();
+                    room.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 2f, 280f, 0.25f, self, 0.7f, 160f, 1f));
+                    room.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+                    room.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+                    room.AddObject(new ExplosionSpikes(room, pos, 14, 30f, 9f, 7f, 170f, color));
+                    room.AddObject(new ShockWave(pos, 330f, 0.045f, 5, false));
+
+                    room.ScreenMovement(pos, default, 1.3f);
+                    room.PlaySound(SoundID.Bomb_Explode, pos);
+                    room.InGameNoise(new Noise.InGameNoise(pos, 9000f, self, 1f));
+                }
+            }
 
         }
 
